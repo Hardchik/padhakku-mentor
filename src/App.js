7e27a93ebda839a066bd6ed264b95540d1d7f7f7
Hardchik <hardik.chhabra.ug20@nsut.ac.in>
2022-03-27 01:30:34 +0530

    google analytics/GTM added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 // import landingPage from './landingPage'
 import './landingPage.css'
 import React from 'react'
-
-// function App() {
-//   return (
-//     <div>
-//       <landingPage />
-//     </div>
-//   );
-// }
+import TagManager from 'react-gtm-module'
+const tagManagerArgs = {
+  gtmId: 'GTM-KT2GFP9'
+}
+TagManager.initialize(tagManagerArgs)
 
 function App() {
   return (
@@ -16,7 +13,7 @@ function App() {
       <div className='logo'><img src='https://padhakku.s3.ap-south-1.amazonaws.com/website_icon_logos/Padhakku+Blue.png' alt='...' /></div>
       <div className='poster'><img src='https://padhakku.s3.ap-south-1.amazonaws.com/website+banners/Resume.gif' alt='...' /></div>
       <div className='content'><p>Lorem Ipsum is dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p></div>
-      <a className='button' href='https://www.google.com/'>CLICK HERE</a>
+      <a className='mentor_button' href='https://www.google.com/'>CLICK HERE</a>
     </div>
   );
 }
